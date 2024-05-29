7b5bffcf2c2db1c4d30c369ab2b6b5d98d82d145
Daisuke AKAGAWA <daisuke1024akagawa@gmail.com>
2024-05-29 22:59:16 +0900

    Update init

--- onnxruntime_utilities/__init__.py
+++ onnxruntime_utilities/__init__.py
@@ -1,3 +1,3 @@
-from .inferencer import inference
+from .inferencer import generate_inputs, inference
 from .loader import load_inference_session
 from .summarize import show_input_summary, show_output_summary
